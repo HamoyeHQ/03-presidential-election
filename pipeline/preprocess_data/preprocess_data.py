cf2b9bc6bda2c43038f107511e3f739a6c9d2081
Mugumya Twarik Harouna <51790725+twarik@users.noreply.github.com>
2020-10-12 18:20:21 +0300

    Update preprocess_data.py

--- pipeline/preprocess_data/preprocess_data.py
+++ pipeline/preprocess_data/preprocess_data.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import numpy as np
+from collections import Counter
+from imblearn.over_sampling import SMOTE
+
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import StratifiedKFold
 from sklearn.feature_selection import RFECV
-from sklearn.preprocessing import MinMaxScaler
 
 def _preprocess_data():
     '''
@@ -16,32 +18,58 @@ def _preprocess_data():
     '''
     dataset_path='./president-1976-2016.csv'
     df = pd.read_csv(dataset_path, error_bad_lines=False)
-    df.drop(['notes','state_po','candidate', 'office','state','writein','state_ic'], axis = 1,inplace=True )
+    df.drop(['office','notes','version','writein','state_ic', 'state_cen','state_po','state_fips'], axis = 1, inplace=True)
     df.dropna(inplace=True)
+    df.drop_duplicates(subset=['year','candidate', 'state'], inplace=True)
 
-    X = df.drop('party', axis=1)
-    target = df['party']
+    candidate = [i for i in df.candidate]
+    year = df.year.tolist()
+    df.candidate = [candidate[i] + '_' + str(year[i]) for i in range(len(year))]
+
+    df['candidatevotes_propotion'] = df['candidatevotes']/df['totalvotes']
+
+    #Transform the dataset from long format to a wide format
+    df_wide = df.pivot(index='candidate', columns='state', values='candidatevotes_propotion').reset_index()
+    df_wide.fillna(0, inplace=True)
+
+    candidate_party = df[['candidate', 'party']].drop_duplicates(subset=['candidate']).reset_index().drop('index', axis=1)
+
+    df2 = pd.merge(df_wide, candidate_party, how='inner', on=['candidate'])
+
+    party = []
+    for party_name in df2.party:
+        if party_name.lower() != 'democrat' or party_name.lower() != 'republican':
+            party.append('others')
+        else:
+            party.append(party_name)
+
+    df2['party'] = party
+
+    parties = {'democrat': 1, 'republican': 2, 'others': 3}
+    df2['party'] = df2['party'].replace(parties)
+
+    #list of candiatates that have won presidential elections since 1976
+    presidents = ['Carter, Jimmy_1976', 'Reagan, Ronald_1980', 'Reagan, Ronald_1984', 'Bush, George H.W._1988',
+                  'Clinton, Bill_1992', 'Clinton, Bill_1996', 'Bush, George W._2000', 'Bush, George W._2004',
+                  'Obama, Barack H._2008', 'Obama, Barack H._2012', 'Trump, Donald J._2016']
+
+    #Create a target variable
+    df2['target'] = df2['candidate'].isin(presidents).astype(int)
+
+    X, y= df2.drop(columns=['candidate','target']), df2['target']
+
+    sm = SMOTE(random_state=42)
+    X, target = sm.fit_sample(X, y.ravel())
 
     rfc = RandomForestClassifier(random_state=101)
     rfecv = RFECV(estimator=rfc, step=1, cv=StratifiedKFold(10), scoring='accuracy')
     rfecv.fit(X, target)
 
     X.drop(X.columns[np.where(rfecv.support_ == False)[0]], axis=1, inplace=True)
-    y = target
 
     #split the data
-    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(X, target, test_size=0.3, random_state=0)
 
-    #Scaling the data
-    scaler = MinMaxScaler()
-    normalised_train_df = scaler.fit_transform(x_train)
-    normalised_train_df = pd.DataFrame(normalised_train_df, columns=x_train.columns)
-
-    normalised_test_df = scaler.transform(x_test)
-    normalised_test_df = pd.DataFrame(normalised_test_df, columns=x_train.columns)
-
-    #split the data
-    X_train, X_test = normalised_train_df, normalised_test_df
     np.save('x_train.npy', X_train)
     np.save('x_test.npy', X_test)
     np.save('y_train.npy', y_train)
